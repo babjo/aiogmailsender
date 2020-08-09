e83c6e224b299c45afa76ccf845078fef496efb0
babjo <kd980311@naver.com>
2020-08-09 23:58:41 +0900

    Support multiple accounts

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst', 'r') as fh:
 
 setuptools.setup(
     name='aiogmailsender',
-    version='0.0.2',
+    version='0.0.3',
     author='babjo',
     url='https://github.com/babjo/aiogmailsender',
     description='Asynchronous Gmail Client',
